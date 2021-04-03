a4e23c07f8aeaf88b204022bebd9b509cc893187
Percy Wilberth Bonett Mendoza <percy96bm@gmail.com>
2021-04-03 17:33:14 +0200

    Código correcto y limpio

--- src/vad.c
+++ src/vad.c
@@ -6,11 +6,11 @@
 #include "pav_analysis.h"
 
 const float FRAME_TIME = 10.0F; /* in ms. */
-const int TRAMAS_VOZ_NO_DECIDIDAS = 1;     
-const int TRAMAS_SILENCIO_NO_DECIDIDAS = 11;
-const int N_init = 10; // Numero de muestras iniciales que cogemos
+const int TRAMES_VEU_NO_DECIDIDES = 1;     
+const int TRAMES_SILENCI_NO_DECIDIDES = 11;
+const int N_init = 10; // Numero de mostras inicials que agafem
 const int alfa1 = 4; // Margen k1 = k0 + alfa1
-const int alfa2 = 10; // Margen k2 = k0 + alfa2
+const int alfa2 = 10; // Margen k2 = k1 + alfa2 = k0 + alfa1 + alfa2
 
 /* 
  * As the output state is only ST_VOICE, ST_SILENCE, or ST_UNDEF,
@@ -64,14 +64,13 @@ Features compute_features(const float *x, int N) {
   vad_data->state = ST_INIT;
   vad_data->sampling_rate = rate;
   vad_data->frame_length = rate * FRAME_TIME * 1e-3;
-  vad_data->k0 = 0;
-  vad_data->k1 = 0;
-  vad_data->k2 = 0;
-  vad_data->last_sample = 0; //Indica ULTIMA TRAMA amb V o S
-  vad_data->nsamples = 0; //Número de TRAMA actual
-  vad_data->last_state = ST_INIT; //Indica ÚLTIM ESTAT (V o S)
-  //vad_data->time_wait = 90; //Referència del silenci real
-  //vad_data->sample_wait = 1440; //time_wait*fm
+  vad_data->k0 = 0; //Potencia mitja inicial
+  vad_data->k1 = 0; //Umbral de desició 1
+  vad_data->k2 = 0; //Umbral de desició 2
+  vad_data->last_sample = 0; //Indica si la última trama va ser V o S
+  vad_data->nsamples = 0; //Número de trama actual
+  vad_data->last_state = ST_INIT; //Indica l'últim estat útil per nosaltres que va estar (V o S)
+
   return vad_data;
 }
 
@@ -80,9 +79,9 @@ VAD_STATE vad_close(VAD_DATA *vad_data) {
    * TODO: decide what to do with the last undecided frames
    */
   VAD_STATE state;
-  if (vad_data->state==ST_MAYBEVOICE)
+  if (vad_data->state==ST_MAYBEVOICE) //Decidim ST_VOICE si penúltim estat fos ST_MAYBEVOICE
     state=ST_VOICE;
-  else if(vad_data->state==ST_MAYBESILENCE)
+  else if(vad_data->state==ST_MAYBESILENCE) //Decidim ST_SILENCE si penúltim estat fos ST_MAYBESILENCE
     state=ST_SILENCE;
   else 
     state=vad_data->state;
@@ -112,100 +111,64 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
 
   switch (vad_data->state) {
   case ST_INIT:
-    /*vad_data->k0 = f.p + vad_data->alfa0;
-    vad_data->state = ST_SILENCE;*/
     if(vad_data->nsamples < N_init){
-      vad_data->k0+=pow(10,f.p/10);
-      //vad_data->k0 = f.p + vad_data->alfa0
-      //vad_data->nsamples += 1;
-    } else {
-      //vad_data->nsamples_silence = 0;
-      vad_data->k0 = 10*log10(vad_data->k0/N_init);
-      //vad_data->k1 = vad_data->k0 + alfa1;
-      //vad_data->k2 = vad_data->k1 + alfa2;
+      vad_data->k0+=pow(10,f.p/10); //Calculem el sumatori de les N_init primeres mostres
+    } else {                        //la funció pow() es de la llibreria math.h
+      vad_data->k0 = 10*log10(vad_data->k0/N_init); //Calculem el valor dela potencia mitja inicial de les N_init primeres mostres
+      vad_data->k1 = vad_data->k0 + alfa1;  //Calcuelm l'umbral k1 = k0 + alfa1
+      vad_data->k2 = vad_data->k1 + alfa2;  //Calcuelm l'umbral k2 = k1 + alfa2
       vad_data->state = ST_SILENCE;
     }
     break;
 
   case ST_SILENCE:
-    if (f.p > vad_data->k0){ //f.p indica la señal de la trama
-      vad_data->state = ST_MAYBEVOICE;
-      vad_data->last_state = ST_SILENCE;
-      vad_data->last_sample = vad_data->nsamples;
-      //vad_data->nsamples_voice = 0;
+    if (f.p > vad_data->k0){ //si la trama actual té més potencia que k0
+      vad_data->state = ST_MAYBEVOICE;  //Decidim que és ST_VOICE
+      vad_data->last_state = ST_SILENCE;  //Penúltim estat passa a aser ST_SILENCE
+      vad_data->last_sample = vad_data->nsamples; //Indiquem quina ha estat la última trama
       }
     break;
 
   case ST_VOICE:
-    if (f.p < vad_data->k0){
-      vad_data->state = ST_MAYBESILENCE;
-      vad_data->last_state = ST_VOICE;
-      vad_data->last_sample = vad_data->nsamples;
-      //vad_data->nsamples_silence = 0;
+    if (f.p < vad_data->k0){  //si la trama actual té menys potencia que k0
+      vad_data->state = ST_MAYBESILENCE;  //Decidim que és ST_MAYBESILENCE
+      vad_data->last_state = ST_VOICE;  //Penúltim estat passa a aser ST_VOICE
+      vad_data->last_sample = vad_data->nsamples; //Indiquem quina ha estat la última trama
     }
     break;
 
   case ST_MAYBESILENCE:
-      /*if(vad_data->nsamples_silence < vad_data->sample_wait){
-        if(f.p > vad_data->k2){
-          vad_data->state = ST_VOICE;
-        }
-        else{
-          vad_data->nsamples_silence += 1;
-        }
-      }else{
-        if(f.p < vad_data->k1){
-          vad_data->state = ST_SILENCE;
-        }
-        else{
-          vad_data->nsamples_silence += 1;
-        }
-      }*/
-      if(f.p > vad_data->k0 + alfa1 + alfa2){ 
-        vad_data->state = ST_VOICE;
-      }
-        else if ((vad_data->nsamples - vad_data->last_sample) == TRAMAS_SILENCIO_NO_DECIDIDAS){
-        vad_data->state = ST_SILENCE;
-      }
+    if(f.p > vad_data->k2){   //si la trama actual té més potencia que k2
+      vad_data->state = ST_VOICE;  //Decidim que és ST_VOICE
+    }
+      else if ((vad_data->nsamples - vad_data->last_sample) == TRAMES_SILENCI_NO_DECIDIDES){
+      vad_data->state = ST_SILENCE;  //Decidim que és ST_SILENCE si portem més de TRAMES_SILENCI_NO_DECIDIDES
+    }                                //en l'estat ST_MAYBESILENCE
     break;
 
   case ST_MAYBEVOICE:
-    /*if(f.p > vad_data->k2){
-      vad_data->state = ST_VOICE;
-    }else if(f.p < vad_data->k1){
-      vad_data->state = ST_SILENCE;
-    }else{
-      vad_data->nsamples_voice += 1;
-    }*/
-    if(f.p < vad_data->k0 + alfa1){
-      vad_data->state = ST_SILENCE;
+    if(f.p < vad_data->k1){   //si la trama actual té menys potencia que k1
+      vad_data->state = ST_SILENCE;  //Decidim que és ST_SILENCE
     }
-    else if ((vad_data->nsamples - vad_data->last_sample) == TRAMAS_VOZ_NO_DECIDIDAS){
-      vad_data->state = ST_VOICE;
-    }
+    else if ((vad_data->nsamples - vad_data->last_sample) == TRAMES_VEU_NO_DECIDIDES){
+      vad_data->state = ST_VOICE; //Decidim que és ST_VOICE si portem més de TRAMES_SILENCI_NO_DECIDIDES
+    }                             //en l'estat ST_MAYBEVOICE
     break;
 
   case ST_UNDEF:
-    /*if(vad_data->last_state == ST_SILENCE){
-      vad_data->state = ST_SILENCE;
-    }else if(vad_data->last_state == ST_VOICE){
-      vad_data->state = ST_VOICE;
-    }*/
     break;
   }
 
   vad_data->nsamples += 1;
 
   if (vad_data->state == ST_SILENCE ||
-      vad_data->state == ST_VOICE ||
-      vad_data->state == ST_MAYBEVOICE ||
-      vad_data->state == ST_MAYBESILENCE){
-    return vad_data->state;
+      vad_data->state == ST_VOICE){
+    return vad_data->state; //Retornem l'estat en el que estem
   }else if (vad_data->state == ST_INIT){
-    return ST_SILENCE;
+    return ST_SILENCE;  //Considerem que els primers segments de la senyal son sempre silenci
   }
   else{
-    return ST_UNDEF;
+    return vad_data->last_state;  //Si l'estat es ST_UNDEF retornem l'últim estat en el que estavem vàlid (V o S)
   }
 }
 
